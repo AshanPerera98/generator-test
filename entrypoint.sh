16b6622590bb735e37b3f04b0a0ea4fcb4af792c
Ashan Perera <52708527+AshanPerera98@users.noreply.github.com>
2024-06-17 10:53:39 +0000

    entrypoint added

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "------------------------------------------"
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global --add safe.directory /github/workspace
+
+python3 /usr/bin/feed.py
+
+git add -A
+git commit -m "feed updated"
+git push --set-upstream origin main
+
+echo "------------------------------------------"
